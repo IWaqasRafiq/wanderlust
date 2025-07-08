6d3c5b09f951fc6ea9eee0079ed99fc8f4bfe5f3
Muhammad Waqas <119754074+IWaqasRafiq@users.noreply.github.com>
2025-07-08 20:08:09 +0500

    Add files via upload

---
+++ init/index.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const Listing = require("../models/listing.js");
+const initData = require("./data.js");
+
+const mongoUrl = "mongodb+srv://wm66179:saifmalik28@cluster0.ti04esi.mongodb.net/crud?retryWrites=true&w=majority";
+mongoose.connect(mongoUrl);
+
+const initDB = async () => {
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log("data was initilized");
+}
+
+initDB();
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
+mongoose.connection.on('connected', function () {
+    console.log("Mongoose is connected");
+});
+mongoose.connection.on('disconnected', function () {
+    console.log("Mongoose is disconnected");
+    process.exit(1);
+});
+mongoose.connection.on('error', function (err) {
+    console.log('Mongoose connection error: ', err);
+    process.exit(1);
+});
+process.on('SIGINT', function () {
+    console.log("app is terminating");
+    mongoose.connection.close(function () {
+        console.log('Mongoose default connection closed');
+        process.exit(0);
+    });
+});
