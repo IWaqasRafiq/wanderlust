bffbbca90dac937e0a419f84785a6e53c019a6d3
Muhammad Waqas <119754074+IWaqasRafiq@users.noreply.github.com>
2025-07-22 20:05:46 +0500

    use express-router for code reusability

--- app.js
+++ app.js
@@ -2,112 +2,81 @@ const express = require("express");
 const app = express();
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
 const path = require("path");
-const methodOverride = require('method-override');
+const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-
-// app.use(express.json());
+const ExpressError = require("./utils/expressError.js");
+const listings = require("./routes/listings.js");
+const reviews = require("./routes/review.js");
+const session = require("express-session");
+const flash = require('connect-flash');
+const passport = require("passport");
+const LocalStrategy =  require('passport-local');
+const User = require('./models/user.js');
+app.use(express.json());
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: true }));
-app.engine('ejs', ejsMate);
+app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-
-const mongoUrl = "mongodb+srv://wm66179:saifmalik28@cluster0.ti04esi.mongodb.net/crud?retryWrites=true&w=majority";
+const mongoUrl =
+  "mongodb+srv://@cluster0.ti04esi.mongodb.net";
 mongoose.connect(mongoUrl);
 
-app.get("/", (req , res) =>{
-    res.send("Root is working.")
-})
 
-// app.get("/testListing", async (req , res) =>{
-//     let sampleListing = new Listing({
-//         title : "My New Villa",
-//         description : "by the beach",
-//         price : 1200,
-//         location : "Karachi, Sindh",
-//         country : "Paksitan",
-//     });
-//     await sampleListing.save();
-//     console.log("sample was saved");
-//     res.send("succesfull testing")
-// });
+const sessionOptions = {
+  secret: "keyboard cat",
+  resave: false,
+  saveUninitialized: true,
+};
 
-app.get("/listings", async (req, res) =>{
-    const allListings = await Listing.find({});
-    res.render("listing/index.ejs", {allListings});
-    // req.send("working")
+app.get("/", (req, res) => {
+  res.send("Root is working.");
 });
 
-app.get("/listings/new", async (req, res) => {
-    res.render("listing/new.ejs");
+app.use(session(sessionOptions));
+app.use(flash());
+
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next)=>{
+  res.locals.success = req.flash("success");
+  next();
 })
 
 
-app.get("/listings/:id", async (req, res) => {
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listing/show.ejs", {listing});
-})
+app.use("/listings", listings);
+app.use("/listings/:id/reviews", reviews);
 
-app.post("/listings", async(req, res) =>{
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-})
-
-app.get("/listings/:id/edit", async (req, res) => {
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listing/edit.ejs", {listing});
-})
-
-app.put("/listings/:id", async (req, res) =>{
-    let { id} = req.params;
-    await Listing.findByIdAndUpdate(id, {...req.body.listing});
-    res.redirect(`/listings/${id}`);
+app.all("/{*any}", (req, res, next) => {
+  next(new ExpressError(404, "Page Not Found"));
 });
 
-app.delete("/listings/:id", async (req, res) =>{
-    let { id} = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "something went wrong" } = err;
+  res.status(statusCode).render("error.ejs", { message });
+  // res.status(statusCode).send(message);
 });
 
 app.listen(8080, () => {
-    console.log(`port is up 8080`)
+  console.log(`port is up 8080`);
 });
-
-
-
-
-
-
-
-
-
 
 ////////////////mongodb connected disconnected events///////////////////////////////////////////////
-mongoose.connection.on('connected', function () {
-    console.log("Mongoose is connected");
+mongoose.connection.on("connected", function () {
+  console.log("Mongoose is connected");
 });
-mongoose.connection.on('disconnected', function () {
-    console.log("Mongoose is disconnected");
-    process.exit(1);
+mongoose.connection.on("disconnected", function () {
+  console.log("Mongoose is disconnected");
+  process.exit(1);
 });
-mongoose.connection.on('error', function (err) {
-    console.log('Mongoose connection error: ', err);
-    process.exit(1);
-});
-process.on('SIGINT', function () {
-    console.log("app is terminating");
-    mongoose.connection.close(function () {
-        console.log('Mongoose default connection closed');
-        process.exit(0);
-    });
+mongoose.connection.on("error", function (err) {
+  console.log("Mongoose connection error: ", err);
+  process.exit(1);
 });
