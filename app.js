9d2b004bebe209c8f3bc581d7b521ea4b3358615
Muhammad Waqas <119754074+IWaqasRafiq@users.noreply.github.com>
2025-08-04 10:34:34 -0700

    strong image using multer and cloudinary

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
+
 const express = require("express");
 const app = express();
-const ejs = require("ejs");
 const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
--- routes/listings.js
+++ routes/listings.js
@@ -5,6 +5,10 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 
 const listingController = require("../controllers/listings.js");
+const multer  = require('multer')
+const {storage} = require('../cloudinary.js')
+const upload = multer({ storage })
+
 
 router.get(
   "/",
@@ -18,10 +22,11 @@ router.get(
   wrapAsync(listingController.showListing)
 );
 
-router.post(
-  "/",
+router.post( 
+  "/", 
   isLoggedIn,
-  validateListing,
+  upload.single('listing[image]'), 
+  // validateListing,
   wrapAsync(listingController.createListing)
 );
 
